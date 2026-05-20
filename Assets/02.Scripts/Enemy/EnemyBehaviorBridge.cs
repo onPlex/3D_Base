5b598905f00d1abc04f49405b0d0243a34078163
gmw_yjh <frosrain@naver.com>
2026-05-21 06:49:38 +0900

    0520 class done

---
+++ Assets/02.Scripts/Enemy/EnemyBehaviorBridge.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+public class EnemyBehaviorBridge : MonoBehaviour
+{
+    [SerializeField] private EnemyConfigSO config;
+    [SerializeField] private Transform[] patrolPoints;
+    public EnemyConfigSO Config => config;
+    public Transform[] PatrolPoints => patrolPoints;
+    public bool HasConfig => config != null;
+    public bool HasPatrolPoints => patrolPoints != null && patrolPoints.Length > 0;
+    public Vector3 GetPatrolPosition(int index)
+
+    {
+        if (!HasPatrolPoints)
+            return transform.position;
+        int safeIndex = Mathf.Abs(index) % patrolPoints.Length;
+        Transform point = patrolPoints[safeIndex];
+        return point != null ? point.position : transform.position;
+    }
+}
